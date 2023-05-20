3f9edce94e07583e67480c70d8b08d4cf834df4f
tomo-ai <57313205+tomo-ai@users.noreply.github.com>
2023-05-20 06:38:42 +0000

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 """JOBS = [
@@ -32,12 +32,21 @@ app = Flask(__name__)
 @app.route("/")
 def hello_jovian():
   jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs, company_name='Jovian')
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(load_jobs_from_db())
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  else:
+    return render_template('jobpage.html', job=job)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -17,3 +17,18 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(row._asdict())
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs WHERE id = :val"),
+      {"val": id}
+    )
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._asdict()
+
+print(load_job_from_db(1))
+
